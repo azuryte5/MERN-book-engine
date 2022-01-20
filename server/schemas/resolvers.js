97c6984d0bac59ea1931d6f0e91725afde35a580
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2022-01-20 09:20:32 -0500

    Setting up first switch to graph, getting run develop working

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,24 @@
+const {User} = require('../models');
+
+const resolvers = {
+Query: {
+    me: async (parent, args, context) => {
+        const userData = await User.findOne({ _id: user._id })
+        .select("-__v, -password");
+        return userData;
+      
+    //   throw new AuthenticationError("Not logged in!");
+    },
+  },
+}
+// Query: {
+//     me: async (parent, args, context) => {
+//       if (context.user) {
+//         const userData = await User.findOne({ _id: context.user._id })
+//         .select("-__v, -password");
+//         return userData;
+//       }
+//     //   throw new AuthenticationError("Not logged in!");
+//     },
+//   },
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+// will turn off to test if working
+const routes = require("./routes");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// will eventually turn off
 app.use(routes);
 
-db.once('open', () => {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
